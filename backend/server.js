221a1f23ab68cf65d49907603688b59b719cafa7
Mohammed Hussain M <159229210+M0hammedhu5ssain@users.noreply.github.com>
2025-06-08 20:42:52 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,9 @@ app.use(express.json());
 const itemRoutes = require('./routes/itemRoutes');
 app.use('/items', itemRoutes);
 
+// PORT should be from Render
+const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => app.listen(5000, () => console.log("Backend running on port 5000")))
+  .then(() => app.listen(PORT, () => console.log(`Backend running on port ${PORT}`)))
   .catch((err) => console.error("MongoDB connection failed:", err));
